1ff1385f30c7e6952c4bc9a31da9cf374c5fb29a
Stanley Masinde <stanmasinde@gmail.com>
2024-03-06 16:02:28 +0300

    fix: share not working for Android
    Android shares all links in the text field

--- src/views/HandleLink.vue
+++ src/views/HandleLink.vue
@@ -3,16 +3,24 @@ import { useRoute } from 'vue-router'
 
 const $route = useRoute()
 const sharedLink: string = $route.query.link as string
-const sanitizedLink = (link?: string) => {
+const sharedText: string = $route.query.link as string
+const sanitizedLink = (link?: string, text?: string) => {
+  let currentLink = ''
+  if (link) {
+    currentLink = link
+  }
+  if (text) {
+    currentLink = text
+  }
   const regex =
     /[?&](utm_[^=]+|pk_[^=]+|fbclid|feature|src|ref|cmp|cmp_id|ref_source|ref_medium|ref_campaign|ref_content|ref_term|gclid|msclkid|yclid)=([^&]+)/gi
-  const cleanLink = link?.replace(regex, '')
+  const cleanLink = currentLink?.replace(regex, '')
   return cleanLink?.replace(/[?&]$/, '')
 }
 const share = async () => {
   if (navigator.share) {
     await navigator.share({
-      url: sanitizedLink(sharedLink)
+      url: sanitizedLink(sharedLink, sharedText)
     })
   }
 }
@@ -24,7 +32,11 @@ const share = async () => {
       {{ $route.query }}
     </div>
     <div class="flex flex-col gap-2 mx-3">
-      <textarea :value="sanitizedLink(sharedLink)" readonly class="w-full rounded-lg"></textarea>
+      <textarea
+        :value="sanitizedLink(sharedLink, sharedText)"
+        readonly
+        class="w-full rounded-lg"
+      ></textarea>
       <button @click.prevent="share" class="border px-4 bg-blue-600 rounded-lg text-white py-2">
         Share
       </button>
