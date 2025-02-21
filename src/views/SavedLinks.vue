f563fc7946f0b3340f7c2d30484202dd74014b72
Stanley Masinde <stanmasinde@gmail.com>
2025-02-21 19:46:17 +0300

    ft: links can be deleted

--- src/views/SavedLinks.vue
+++ src/views/SavedLinks.vue
@@ -9,7 +9,27 @@ interface SavedLink {
 
 const savedLinks: Ref<SavedLink[]> = ref([])
 
-onMounted(() => {
+// Delete Link
+const deleteLink = (id: IDBValidKey) => {
+	const openDbRequest = indexedDB.open('linksDb', 1)
+
+	openDbRequest.onsuccess = (event) => {
+		const db = (event.target as IDBOpenDBRequest)?.result as IDBDatabase
+
+		const transaction = db.transaction('links', 'readwrite')
+		const objectStore = transaction.objectStore('links')
+
+		const deleteOperation = objectStore.delete(id)
+		deleteOperation.onsuccess = (ev) => {
+			console.log(ev)
+			fetchSavedNotes()
+		}
+	}
+}
+
+// Get all the saved link
+const fetchSavedNotes = () => {
+	savedLinks.value = []
 	const request = indexedDB.open('linksDb', 1)
 
 	request.onsuccess = (event) => {
@@ -33,11 +53,15 @@ onMounted(() => {
 	}
 
 	request.onerror = () => console.error('Error opening IndexedDB')
+}
+
+onMounted(() => {
+	fetchSavedNotes()
 })
 </script>
 
 <template>
-	<main class="grid grid-cols-12 grid-rows-1 mx-2 md:mx-48">
+	<main class="grid grid-cols-12 grid-rows-1 mx-1 md:mx-48">
 		<div class="col-span-12 w-full">
 			<div class="rounded-lg p-5">
 				<div class="flex justify-center mb-2 w-full sticky top-0 bg-white p-2">
@@ -56,13 +80,21 @@ onMounted(() => {
 				<!-- The list of links -->
 				<div data-cy="saved-link-item ">
 
-					<div class="text-center">No links found!</div>
+					<div class="text-center" v-if="savedLinks.length == 0">No links found!</div>
 
-					<div v-for="(link, index) in savedLinks" class="border rounded-lg p-2 m-4 overflow-x-auto"
-						:key="index">
-						<div>
+					<div v-else v-for="(link, index) in savedLinks" class="border rounded-lg p-2 m-4" :key="index">
+						<div class="line-clamp-1">
 							<p class="text-lg">{{ link.link.url }}</p>
+						</div>
+
+						<div>
 							<small class="text-xs font-semibold">{{ new Date(link.link.createdAt) }}</small>
+						</div>
+
+						<!-- Actions -->
+						<div class="flex flex-col mt-2">
+							<button @click.prevent="deleteLink(link.id)"
+								class="bg-red-500 text-white rounded-lg py-1">Delete</button>
 						</div>
 					</div>
 				</div>
