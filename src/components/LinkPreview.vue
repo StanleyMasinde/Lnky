9f67c684c536ad3db94998cf9fda58c9c0dbd5a8
Stanley Masinde <stanmasinde@gmail.com>
2025-05-16 16:11:16 +0300

    ft: update the UI and request mode

--- src/components/LinkPreview.vue
+++ src/components/LinkPreview.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watchEffect } from 'vue'
 
 const props = defineProps<{
 	url: string
@@ -9,8 +9,11 @@ const title = ref<string | undefined>()
 const image = ref<string | undefined>()
 const description = ref<string | undefined>()
 
-onMounted(async () => {
-	const res = await fetch(props.url)
+watchEffect(async () => {
+	if (!props.url) return
+	const res = await fetch(props.url, {
+		mode: 'cors',
+	})
 	const htmlRes = await res.text()
 	const domParser = new DOMParser()
 	const parsed = domParser.parseFromString(htmlRes, 'text/html')
@@ -27,17 +30,17 @@ onMounted(async () => {
 </script>
 
 <template>
-	<div class="link-preview flex items-center space-x-4 p-4 border rounded-lg shadow-md max-w-md">
+	<div class="link-preview flex items-center space-x-4 p-4 border rounded-lg shadow-md w-full overflow-y-scroll">
 		<img v-if="image" :src="image" alt="Preview Image" class="w-20 h-20 rounded-md object-cover" />
 		<div class="flex flex-col flex-1">
 			<h1 id="title" class="font-semibold text-lg line-clamp-2">{{ title || 'Title not available' }}</h1>
 			<p id="description" class="text-sm text-gray-600 line-clamp-3">{{ description || 'Description not available'
 			}}</p>
-			<div class="line-clamp-1">
-				<a class="text-lg underline hover:text-primary" :href="url" target="_blank">
-					{{ url }}
-				</a>
-			</div>
+
+			<a class="text-lg underline hover:text-primary line-clamp-1" :href="url" target="_blank">
+				{{ url }}
+			</a>
+
 		</div>
 	</div>
 </template>
